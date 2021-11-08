8b5e8061534e6935e4964658bfbc21c0a5e89dab
Reut Dayan <reut.dayan1@live.biu.ac.il>
2021-11-08 22:25:15 +0200

    start detect func

--- timeseries.cpp
+++ timeseries.cpp
@@ -1,7 +1,10 @@
 #include "timeseries.h"
 #include <string.h>
 
-int TimeSeries::getNumberOfColumns() const {return 1;}
-float* TimeSeries::getVectorFeature(int i) const{}
+int TimeSeries::getNumberOfColumns() const {}
+
+float *TimeSeries::getVectorFeature(int i) const {}
+
 int TimeSeries::getNumberOfRows() const {}
-string TimeSeries::getFeatureName(int i) const{}
+
+string TimeSeries::getFeatureName(int i) const {}
--- timeseries.h
+++ timeseries.h
@@ -1,24 +1,23 @@
-
-
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
 
 using namespace std;
 
-class TimeSeries{
+class TimeSeries {
 
 public:
 
-	TimeSeries(const char* CSVfileName){
-	}
+    TimeSeries(const char *CSVfileName) {
+    }
 
-	virtual int getNumberOfColumns() const;
-    virtual float* getVectorFeature(int i) const;
+    virtual int getNumberOfColumns() const;
+
+    virtual float *getVectorFeature(int i) const;
+
     virtual int getNumberOfRows() const;
+
     virtual string getFeatureName(int i) const;
 
 };
-
-
 
 #endif /* TIMESERIES_H_ */
